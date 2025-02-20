f5b6158a1f7545dcb7d7cf2e66daaa4e00c36cd7
Luann8 <luannpmendes@gmail.com>
2025-02-20 10:47:44 -0300

    Criei o usuario de teste

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -1,20 +1,17 @@
-'use strict'
-
-/*
-|--------------------------------------------------------------------------
-| DatabaseSeeder
-|--------------------------------------------------------------------------
-|
-| Make use of the Factory instance to seed database with dummy data or
-| make use of Lucid models directly.
-|
-*/
-
 /** @type {import('@adonisjs/lucid/src/Factory')} */
 const Factory = use('Factory')
+const User = use('App/Models/User') // Importando o modelo de usuário
 
 class DatabaseSeeder {
-  async run () {
+  async run() {
+    // Criar um usuário manualmente
+    const user = await User.create({
+      username: 'usuario_exemplo',
+      email: 'usuario@example.com',
+      password: 'senha123', // Certifique-se de que o modelo User tem um hook para hash da senha
+    })
+
+    console.log('Usuário criado:', user.toJSON())
   }
 }
 
